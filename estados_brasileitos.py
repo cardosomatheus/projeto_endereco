c37a409b46a8ef9ed06b4b69d050d654bd47db0b
matheus <cardosomcds1@gmail.com>
2023-04-06 11:02:40 -0300

    1.0 v do projeeto: ETL e insercao de enderecos

--- estados_brasileitos.py
+++ estados_brasileitos.py
@@ -1,8 +1,8 @@
+#   Bibliotecas
 import requests
-from random import randint
 from geopy.geocoders import Nominatim
 
-
+#   Classes
 class Estados_Municipios_Brasileiros:
     def __init__(self):
         self.requisicao_uf = requests.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
@@ -10,17 +10,16 @@ class Estados_Municipios_Brasileiros:
         self.requisicao_mun = requests.get(self.link_municipio)
 
 
-
     def informacoes_estados_txt(self):
         # Retorna cod_ibge,sigla_uf,estado e regiao de todos os estados do Brasil escritos em TXT
         with open ('estados_uf.txt','w') as uf:
             for estado in self.requisicao_uf.json():
-                cod_ibge = estado["id"]
-                sigla_uf = estado["sigla"]
-                nome_uf   = estado["nome"]
-                regiao   = estado["regiao"]["nome"]
+                cod_ibge,sigla_uf,nome_uf,regiao = estado["id"],estado["sigla"],estado["nome"],estado["regiao"]["nome"]
+
+                # o print e escrita das informações dos estados brasileiros
                 print(f'{sigla_uf},{cod_ibge},{nome_uf},{regiao}')
                 uf.write(f'{sigla_uf},{cod_ibge},{nome_uf},{regiao}\n')
+
 
     def id_estados(self):
         # retorna todos os ID dos estados brasileiros na variavel id_estados
@@ -29,30 +28,28 @@ class Estados_Municipios_Brasileiros:
             id_estados += f'{estado["id"]}|'
         return id_estados[:-1]   
     
+    
     def municpios_por_estado(self):
         # retorna um txt com sigla_uf,cod_ibge,nome_municpio,localidade dos muicipios
         with open('municipios.txt','w') as municpios_txt:
             for municipio in self.requisicao_mun.json():
                 sigla_uf = f'{municipio["mesorregiao"]["UF"]["sigla"]}'
-                cod_ibge = municipio['id']
-                nome_municpio = municipio['nome']
+                cod_ibge,nome_municpio = municipio['id'], municipio['nome']
     
                 try:
                     # Faz a busca através do sigla_uf, cod_ibge, nome_municpio retornada em dict
                     geolocator = Nominatim(user_agent="test_app")
                     location = geolocator.geocode(f'{sigla_uf}, {cod_ibge}, {nome_municpio}')
-                    latitude = location.latitude
-                    longitude = location.longitude  
+                    latitude, longitude= location.latitude, location.longitude  
 
-                    # armazenados em uma arquivo txt
+                    # o print e escrita das informações dos estados brasileiros
                     print(f'{sigla_uf},{cod_ibge},{nome_municpio},{latitude},{longitude}')
                     municpios_txt.write(f'{sigla_uf},{cod_ibge},{nome_municpio},{latitude},{longitude}\n') 
                 except: 
-                    # Em caso de error ele passa o municipio 
+                    # Em caso de error ele passa o municipio e não insere o arquivo txt
                     pass
     
 
 estados = Estados_Municipios_Brasileiros ()       
-    
 estados.informacoes_estados_txt()
-#estados.municpios_por_estado()
+estados.municpios_por_estado()
