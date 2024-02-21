1fe56d67998e1f58aa9f738e063bc5387a864694
matheus <cardosomcds1@gmail.com>
2024-02-21 10:30:39 -0300

    Processo de carga dos estados brasileiros.

--- pasta_python/engine_banco.py
+++ pasta_python/engine_banco.py
@@ -3,10 +3,3 @@ import oracledb
 
 conexao  = oracledb.connect(user="userhr", password='mcds123',
                               host="localhost", port=1521, service_name="XEPDB1")
-
-
-
-with conexao.cursor() as conn:
-    sql = 'select * from tb_moradores'
-    for linha in conn.execute(sql):
-        print('linha', linha)
